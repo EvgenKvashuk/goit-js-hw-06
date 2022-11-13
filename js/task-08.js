7f0ceede15c4eaeb830c671536b14fb107368a0e
EvgenKvashuk <105291436+EvgenKvashuk@users.noreply.github.com>
2022-11-13 10:40:26 +0200

    add

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', onFormSubmit);
 
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+
+    for (const [name, value] of formData.entries()) {
+        if (value.trim() === "") {
+            return alert("всі поля повинні бути заповнені");
+        }
+    }
+    
+    const dataObj = {};
+
+    for (let element of event.currentTarget.elements) {
+        if (element.name) dataObj[element.name] = element.value;
+      }
+    console.log(dataObj);
+    event.currentTarget.reset();
+}
